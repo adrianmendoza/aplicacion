afe742dfc3677f357455ec583e2932ff3574af13
adrianmendoza <marcelomendoza@hotmail.es>
2015-07-11 23:07:55 -0500

    Revert "Aplicacion"
    This reverts commit b501a20100f76a996c8efa443300dc0b67a793dd.

--- app/views/email.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Bienvenido al sistema de creacion de planes de negocio</h1>
-
-<p> Estimado usuario para activar su usuario haga clic en el siguiente vinculo {{ link_to('activar/'.$identificador,'Haga clic aqui')}}
-</p>
